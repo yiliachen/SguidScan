d1be4d5903e4913fc1c49959d2fb79abd58c2d44
ming.c.chen <ming.c.chen@dhcp-10-182-161-139.cn.oracle.com>
2018-05-11 10:54:52 +0800

    add new rowmapper

---
+++ src/main/java/com/example/model/rowmapper/RowDifferMapper.java
@@ -0,0 +1,17 @@
+package com.example.model.rowmapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class RowDifferMapper  implements RowMapper<RowDifferMapper> {
+
+	//TODO design and implement getter and setter of attribute of this mapper
+	@Override
+	public RowDifferMapper mapRow(ResultSet rs, int rowNum) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
